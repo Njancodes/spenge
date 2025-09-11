a3bad95ae6ea460b3286827f880dbe7d61954c16
njancodes <njansmailbox@gmail.com>
2025-09-11 07:17:48 +0530

    Distingush file formats

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include <dirent.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <regex.h>
+
+enum fformat {
+	TEXT,
+	IMAGE,
+	ERREXT
+};
 
 char* validDirectory(char *dirpath){
 	if(dirpath[0] != '/' && dirpath[0] != '.'){
@@ -16,12 +23,35 @@ char* validDirectory(char *dirpath){
 	return dirpath;
 }
 
+enum fformat getfformat(const char *filename){
+	char *ext = strrchr(filename, '.');
+	enum fformat ff = ERREXT;
+	
+	regex_t textregex;
+	regex_t imageregex;
+
+	int text_reg_errval = regcomp(&textregex,".txt|.c|.py|.java|.rs",REG_EXTENDED);
+	int img_reg_errval = regcomp(&imageregex,".png|.jpg",REG_EXTENDED);
+
+	int text_file_check = regexec(&textregex,ext,0,NULL,0);
+	int image_file_check = regexec(&imageregex,ext,0,NULL,0);
+
+	if(text_file_check != REG_NOMATCH){
+		ff = TEXT;
+	}
+	if(image_file_check != REG_NOMATCH){
+		ff = IMAGE;
+	}
+
+	return ff;
+}
+
 int main(int argc, char *argv[]){
 	printf("The number of arguments is: %d\n",argc);
 
 	DIR *d;
 	FILE *f;
-	struct dirent *dir;
+	struct dirent *dirent;
 	int count = 0;
 	int ch;
 	char *dirpath;
@@ -33,31 +63,47 @@ int main(int argc, char *argv[]){
 	opath = malloc(sizeof(dirpath));
 	strcpy(opath, dirpath);
 	printf("The directory path is: %s\n", dirpath);
+
 	if((d = opendir(dirpath)) != NULL){
-		while((dir = readdir(d)) != NULL) {
-			if(dir->d_type == DT_REG){
+		while((dirent = readdir(d)) != NULL) {
+			if(dirent->d_type == DT_REG){
+				const char *filepath = strcat(dirpath, dirent->d_name);
+				const char *filename = dirent->d_name;
+				enum fformat fileformat = getfformat(filename);
+
+				printf("Debug-------------------------------------------");
 				printf("Directory name: %s\n",opath);
-				char *filepath = strcat(dirpath, dir->d_name);
+				printf("File format: %d\n",fileformat);
 				printf("File path: %s\n",filepath);
-				printf("File name: %s\n",dir->d_name);
-				f = fopen(filepath, "r");
-				printf("\n");
-				while((ch  = getc(f)) != EOF){
-					printf("%c",ch);
+				printf("File name: %s\n",filename);
+				printf("Debug-------------------------------------------");
+				
+				if(fileformat == TEXT){
+					f = fopen(filepath, "r");
+					printf("\n");
+				
+					while((ch  = getc(f)) != EOF){
+						printf("%c",ch);
+					}
+
+					fclose(f);
+				}else if(fileformat == IMAGE){
+					printf("\nThis is an image file.\nNo Support for it yet\n");
 				}
 				
-				printf("\n Do you want to delete (X) or save (S) this file (%s): ",dir->d_name);
+				printf("\n Do you want to delete (X) or save (S) this file (%s): ",filename);
 				scanf("%c",&choice);
 				getchar();
 				if(toupper(choice) == 'X'){
-					printf("\nThe file (%s) is to be deleted", dir->d_name);
+					printf("\nThe file (%s) is to be deleted", filename);
 				}else if(toupper(choice) == 'S'){
-					printf("\nThe file (%s) is to be saved",dir->d_name);
+					printf("\nThe file (%s) is to be saved",filename);
 				}else{
 					printf("\nPlease give an appropriate response");
 				}
 				printf("\n");
-				fclose(f);	
+				
+				
 				strcpy(dirpath,opath);
 			}
 			count += 1;
